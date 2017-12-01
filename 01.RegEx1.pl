f9e2057a81fdd08141211d1679f877f196f31b25
chetanDN <chetandevanaik@gmail.com>
2017-12-01 09:45:20 +0530

    Update 01.RegEx1.pl

--- 01.RegEx1.pl
+++ 01.RegEx1.pl
@@ -1,9 +1,15 @@
 $bar = "this is myfoodworld and again foo";
-if ($bar =~ /foo/) {
+
+$status = ($bar =~ /foo/);
+print "Status = $status\n"; #match success = 1, failure = 0;
+
+if ($status) {
 	print "$`"." "."$&"." "."$'";
 } else {
 	print "not matching";
 }
+
+
 
 =begin result
 this is my foo dworld and again foo
